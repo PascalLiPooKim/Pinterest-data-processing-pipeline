e61166e2cddb6ebbac5c4aad1997e0770693ecd7
Pascal Li Poo Kim <paswei98@gmail.com>
2022-02-28 20:44:30 +0000

    Manage to send data HBase

--- Pinterest_Batch_Processing/pinterest_df_spark_to_hbase.py
+++ Pinterest_Batch_Processing/pinterest_df_spark_to_hbase.py
@@ -9,17 +9,17 @@ def write_df_to_hbase(df):
 
     # --master yarn-client --packages com.hortonworks:shc-core:1.1.1-2.1-s_2.11 --repositories http://nexus-private.hortonworks.com/nexus/content/repositories/IN-QA/
 
-    try:
-        # os.environ['PYSPARK_SUBMIT_ARGS'] = '--master yarn-client \
-        #     --packages com.hortonworks:shc:1.1.1-2.1-s_2.11 \
-        #     --repositories http://repo.hortonworks.com/content/groups/public/ \
-        #     --files /home/aicore/hbase-1.7.1/conf/hbase-site.xml \
-        #     pinterest_df_spark_to_hbase.py pyspark-shell'
-        os.environ['PYSPARK_SUBMIT_ARGS'] = '--packages it.nerdammer.bigdata:spark-hbase-connector_2.10:1.0.3 \
-            --repositories git@github.com:nerdammer/spark-hbase-connector \
-            pinterest_df_spark_to_hbase.py pyspark-shell'
-    except:
-        print('Packages not required')
+    # try:
+    #     # os.environ['PYSPARK_SUBMIT_ARGS'] = '--master yarn-client \
+    #     #     --packages com.hortonworks:shc:1.1.1-2.1-s_2.11 \
+    #     #     --repositories http://repo.hortonworks.com/content/groups/public/ \
+    #     #     --files /home/aicore/hbase-1.7.1/conf/hbase-site.xml \
+    #     #     pinterest_df_spark_to_hbase.py pyspark-shell'
+    #     os.environ['PYSPARK_SUBMIT_ARGS'] = '--packages it.nerdammer.bigdata:spark-hbase-connector_2.10:1.0.3 \
+    #         --repositories git@github.com:nerdammer/spark-hbase-connector \
+    #         pinterest_df_spark_to_hbase.py pyspark-shell'
+    # except:
+    #     print('Packages not required')
 
     df = df.select('unique_id', 'category', 'title', 'follower_count')
 
@@ -48,17 +48,26 @@ def write_df_to_hbase(df):
     # dataSourceFormat = "org.apache.spark.sql.execution.datasources.hbase"
     dataSourceFormat = "org.apache.hadoop.hbase.spark"
 
-    df.write\
-    .options(catalog=catalog)\
-    .format(dataSourceFormat)\
-    .save()
     # df.write\
     # .options(catalog=catalog)\
-    # .option("hbase.spark.use.hbasecontext", False)\
-    # .option("hbase.table", "pinterest_table")\
+    # .format(dataSourceFormat)\
+    # .save()
+    df.write\
+    .option("hbase.spark.use.hbasecontext", False)\
+    .option("hbase.table", "pinterest_table")\
+    .option("hbase.columns.mapping", 
+            "unique_id STRING :key , \
+            category STRING cf:category, \
+            title STRING cf:title, \
+            follower_count STRING cf:followers_count") \
+    .option("hbase.spark.pushdown.columnfilter", False) \
+    .format(dataSourceFormat) \
+    .save()
+    
+    df.show()
+    
     # .option('hbase.config.resources', 'file:////home/aicore/hbase-1.7.1/conf/hbase-site.xml') \
     # .format("org.apache.spark.sql.execution.datasources.hbase")\
-    # .save()
 
     # .format("org.apache.hadoop.hbase.spark")\
 
